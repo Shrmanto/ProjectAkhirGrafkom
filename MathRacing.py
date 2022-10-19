f1cd35d0bd18c96fd90025568602d243f087d643
Shrmanto <shrmantoch@gmail.com>
2022-10-19 08:13:30 +0700

    Awal Background

--- MathRacing.py
+++ MathRacing.py
@@ -3,3 +3,7 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
 w,h= 1200,600
+
+def background():
+    glBegin(GL_POLYGON)
+    glEnd()
--- ProjectAkhir/MathRacing.py
+++
@@ -1,5 +0,0 @@
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-from OpenGL.GLU import *
-
-w,h= 1200,600
