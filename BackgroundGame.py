aea8621d0e2383522bb48ebed87ff897cc8e6428
Shrmanto <shrmantoch@gmail.com>
2022-10-29 09:06:56 +0700

    Background

--- BackgroundGame.py
+++ BackgroundGame.py
@@ -5,6 +5,7 @@ from OpenGL.GLU import *
 w,h= 1280,720
 
 def background():
+    #LANGIT
     glColor3ub(135, 206, 235)
     glBegin(GL_QUADS)
     glVertex2f(0, 0)
@@ -13,11 +14,45 @@ def background():
     glVertex2f(1280, 0)
     glEnd()
 
+    #JALAN
+    glColor3ub(105, 105, 105)
+    glBegin(GL_QUADS)
+    glVertex2f(0, 210)
+    glVertex2f(1280, 210)
+    glVertex2f(1280, 0)
+    glVertex2f(0, 0)
+    glEnd()
+
+    #RUMPUT
+    glColor3ub(50, 205, 50)
+    glBegin(GL_QUADS)
+    glVertex2f(0, 120)
+    glVertex2f(1280, 120)
+    glVertex2f(1280, 100)
+    glVertex2f(0, 100)
+    glEnd()
+    glColor3ub(50, 205, 50)
+    glBegin(GL_QUADS)
+    glVertex2f(0, 220)
+    glVertex2f(1280, 220)
+    glVertex2f(1280, 200)
+    glVertex2f(0, 200)
+    glEnd()
+    glColor3ub(50, 205, 50)
+    glBegin(GL_QUADS)
+    glVertex2f(0, 20)
+    glVertex2f(1280, 20)
+    glVertex2f(1280, 0)
+    glVertex2f(0, 0)
+    glEnd()
+
+
+
 def iterate():
     glViewport(0, 0, 1280, 720) 
     glMatrixMode(GL_PROJECTION) 
     glLoadIdentity()
-    gluOrtho2D(-1280, 1280, -720, 720) 
+    gluOrtho2D(0, 1280, 0, 720) 
     glMatrixMode (GL_MODELVIEW) 
     glLoadIdentity()
 
